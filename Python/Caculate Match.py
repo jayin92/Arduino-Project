31f25fb52ac7b123f54f19f726686ec774d314c2
jayin92 <jayin920805@gmail.com>
2015-11-14 07:45:11 +0800

    new

---
+++ Python/Caculate Match.py
@@ -0,0 +1,14 @@
+while(1):
+    Match1 = []
+    Match2 = []
+    for i in range(7):
+        number1 = input("Please enter " + str(i)+ " number(the first one)")
+        Match1.append(number1)
+    print(" ")
+    for x in range(7):
+        number2 = input("Please enter " + str(i)+ " number(the second one)")
+        Match2.append(number2)
+    for item in Match1:
+         Match1[item]
+    
+            
