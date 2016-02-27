57cafe1e34b69980180d779afb1fb490a9f59bb6
Jay Li <jayin920805@gmail.com>
2016-02-27 13:31:13 +0800

    add

--- Python/PrimeDistinguish/PrimeDistinguish.py
+++ Python/PrimeDistinguish/PrimeDistinguish.py
@@ -2,22 +2,22 @@ import os
 import csv
 import math
 
-with open('primary.txt', 'r') as f:
-    primary = [line.strip() for line in f]
-primary = list(map(int, primary))
-number = max(primary)+1
+with open('prime.txt', 'r') as f:
+    prime = [line.strip() for line in f]
+prime = list(map(int, prime))
+number = max(prime)+1
         
-print(primary)
+print(prime)
 print(number)
 status_number = 1
 confrim_number = 1
 i = 0
 
 while(1):
-       
-    i = min(primary, key = lambda x:abs(x-number))   
-    number2 = primary.index(i)    
-    for x in primary[0:number2]:
+    number1 = int(math.sqrt(number))  
+    i = min(prime, key = lambda x:abs(x-number1))   
+    number2 = prime.index(i)+1    
+    for x in prime[0:number2]:
         if status_number:
             if number % x > 0:
                 status_number = 1
@@ -25,18 +25,18 @@ while(1):
                 status_number = 0
                 confrim_number = 1        
     if confrim_number == 0:
-        primary.append(number)
+        prime.append(number)
         number = number + 1
         os.system('cls')
-        print(primary)
+        print(max(prime))
     if confrim_number == 1:
         number = number + 1
         status_number = 1
         confrim_number = 0
-    if len(primary) % 100 == 0:
-        file = open('primary.txt', 'w')
+    if len(prime) % 10 == 0:
+        file = open('prime.txt', 'w')
         file.truncate()
-        for item in primary:
+        for item in prime:
             file.write("%s\n" % item)
         file.close()            
     
