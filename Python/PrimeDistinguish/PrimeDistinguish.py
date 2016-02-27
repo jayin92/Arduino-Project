9635eb4742172fb3fd88c5d2ae9055f3fe4d8f50
Jay Li <jayin920805@gmail.com>
2016-02-27 08:11:37 +0800

    add new code

--- Python/PrimeDistinguish/PrimeDistinguish.py
+++ Python/PrimeDistinguish/PrimeDistinguish.py
@@ -1,13 +1,47 @@
+import os
+import csv
+import math
+
+with open('primary.txt', 'r') as f:
+    primary = [line.strip() for line in f]
+primary = list(map(int, primary))
+number = max(primary)+1
+        
+print(primary)
+print(number)
+status_number = 1
+confrim_number = 1
+i = 0
+
 while(1):
-    number = int(input("type number =>"))
-    number1 = 1
-    for x in range(1,number):
-        number1 = number1 * x
-    if(number == 1):
-        print("1 is not prime number, but it's not composite number, too.")         
-    if(number1 % number == number - 1 & number > 1):
-        print("Prime number")
-    if not(number1 % number == number - 1):
-        print("Composite number")
+       
+    i = min(primary, key = lambda x:abs(x-number))   
+    number2 = primary.index(i)    
+    for x in primary[0:number2]:
+        if status_number:
+            if number % x > 0:
+                status_number = 1
+            if number % x == 0:
+                status_number = 0
+                confrim_number = 1        
+    if confrim_number == 0:
+        primary.append(number)
+        number = number + 1
+        os.system('cls')
+        print(primary)
+    if confrim_number == 1:
+        number = number + 1
+        status_number = 1
+        confrim_number = 0
+    if len(primary) % 100 == 0:
+        file = open('primary.txt', 'w')
+        file.truncate()
+        for item in primary:
+            file.write("%s\n" % item)
+        file.close()            
+    
+    
 
 
+            
+    
